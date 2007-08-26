fceabc14eb637dd692e2aaf314be5366218179f6
Bernhard Loos <nhuh.put@web.de>
2007-08-26 01:36:36 +0000

    fixed some compiler errors thanks to Sevenhill for the pointer
    svn path=/trunk/kdesupport/akode/; revision=704716

--- lib/akodelib.h
+++ lib/akodelib.h
@@ -34,9 +34,9 @@
 #include <inttypes.h>
 #else
 typedef signed char int8_t;
-typedef unsigned char int8_t;
+typedef unsigned char uint8_t;
 typedef signed short int16_t;
-typedef unsigned short int16_t;
+typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
 typedef signed long long int64_t;
